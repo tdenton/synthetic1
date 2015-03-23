ab265e733ce4730f4e1ccc583ee4e7e912cf6e41
tdenton <tdenton@yahoo.com>
2015-03-23 10:32:23 -0400

    move stuff

--- flask_serve.py
+++ flask_serve.py
@@ -10,19 +10,8 @@ app._static_folder = "dist"
 @app.route("/")
 @app.route("/<path:path>")
 def index(path="index.html"):
-	url = request.url_root
-	url_pieces = url.split(".")
-	if len(url_pieces) != 3: # subdomain.yorn.com
-		# Some error?  Some default.
-		subdomain = ""
-	else:
-		subdomain = url_pieces[0][url.find("//")+2:]
-	# We have a special short-term case involving 'apps.yorn.com'
-	if subdomain == "client" or subdomain == "go6-yorn":
-		return app.send_static_file(path)
-	else:
-		return app.send_static_file(os.path.join(subdomain, path))
-	#return render_template('index.html')
+    return app.send_static_file(path)
+
 
 if __name__ == "__main__":
 	host = '0.0.0.0'
